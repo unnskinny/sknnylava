312fbb12b4659f7e6a3cb149de2aa58a0f680f82
Sknny <82070946+unnskinny@users.noreply.github.com>
2024-03-21 23:55:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://github.com/unnskinny/sknnylava.git)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-482092bc-aa17-4f2c-8b01-86046616d9b4/)
 
 
 echo "$curl_output" >> Update.tsx
