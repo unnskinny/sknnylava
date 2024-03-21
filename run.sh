dc33d22763017f58d165f7257714992366eef028
Sknny <82070946+unnskinny@users.noreply.github.com>
2024-03-21 23:51:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://github.com/unnskinny/sknnylava.git)
 
 
 echo "$curl_output" >> Update.tsx
